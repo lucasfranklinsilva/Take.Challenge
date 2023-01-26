df8f0e0886ffe1f5026f4fe2497c986ec068977b
Lucas Franklin <lucasfranklin16@gmail.com>
2023-01-26 14:27:19 -0300

    Add language to filter in constants

--- Api/src/GitHubIntegration.Models/ConstantsGithub.cs
+++ Api/src/GitHubIntegration.Models/ConstantsGithub.cs
@@ -8,5 +8,6 @@
         public const string SEARCH_USER_REP_QUERY = "?sort={sort}&direction={direction}";
         public const string VALUE_FIELD_SORT = "{sort}";
         public const string VALUE_FIELD_DIRECTION = "{direction}";
+        public const string FILTER_LANGUAGE = "C#";
     }
 }
